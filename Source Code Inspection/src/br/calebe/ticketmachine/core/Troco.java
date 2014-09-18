11b788fcc05286eed0b8d3ff4be1af630877278c
41182391 <41182391@087877-P33S105.macklabs.br>
2014-09-17 22:26:15 -0300

    Teste de alteração de código (GitHub)

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -10,7 +10,7 @@ class Troco {
 
     protected PapelMoeda[] papeisMoeda;
 
-    public Troco(int valor) {
+    public void Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
         int count = 0;
         while (valor % 100 != 0) {
